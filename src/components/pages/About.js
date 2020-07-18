45f096b10c6b474414f482433652c0fce49537af
Shruti Rao <shrutirao161298@gmail.com>
2020-07-18 19:11:11 +0530

    First commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is the TodoList app v1.0.0. Created by Shruti. It is my first react website</p>
+    </React.Fragment>
+  )
+}
+
+
+export default About;
